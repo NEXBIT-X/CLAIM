fafbd44c143a3cb777efb879820e75f0d6002cc2
Pavan S <pavan03062006@gmail.com>
2025-07-23 14:57:25 +0530

    Update App.jsx

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,46 +1,57 @@
 import './App.css'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Link } from "react-router-dom";
-import logo from './assets/logo.svg'
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { useState } from 'react';
 import Home from './pages/Home.';
 import Dash from './pages/Dash';
 import Patent from './pages/Patent';
 import Login from './pages/Login';
 
-function Nav(){
-  return(
-    <>
-    <nav>
-      <div className='move'>
-      <Link to="/">Home</Link>
-      <Link to="/patents">Patent</Link>
-      <Link to="/login">Login</Link>
-      <Link to="/profile">Profile</Link>  
+function SideNav({ isOpen, toggleNav }) {
+  return (
+    <div className={`side-nav ${isOpen ? 'open' : ''}`}>
+      <div className="nav-links">
+        <Link to="/" onClick={toggleNav}>Home</Link>
+        <Link to="/patents" onClick={toggleNav}>Patent</Link>
+        <Link to="/login" onClick={toggleNav}>Login</Link>
+        <Link to="/profile" onClick={toggleNav}>Profile</Link>
+        <Link to="/logout" onClick={toggleNav}>Log out</Link>
       </div>
-      
-      </nav>
-    </>
-  )
+    </div>
+  );
 }
 
 function App(){
+  const [navOpen, setNavOpen] = useState(false);
+  const toggleNav = () => setNavOpen(prev => !prev);
+  
+  // Closes the side navigation if it's open when clicking the page container
+  const closeNav = () => {
+    if(navOpen) {
+      setNavOpen(false);
+    }
+  };
 
   return (
-    <>
-   <Router>
-   <Nav/>
-   <Routes>
-   <Route path="/" element={<Home/>}/>
-    <Route path="/login" element={<Login/>}/>
-    <Route path="/profile" element={<Dash/>} />
-  
-    <Route path="/patents"element={<Patent/>}/>
-    <Route path="/login" element={<Login/>}/>
-   </Routes>
-   
-   </Router>
-   </>
+    <Router>
+      <header>
+        <button className="hamburger" onClick={toggleNav}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+      </header>
+      <SideNav isOpen={navOpen} toggleNav={toggleNav} />
+      <div className="page-container" onClick={closeNav}>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/dash" element={<Dash/>}/>
+          <Route path="/patents" element={<Patent/>}/>
+          <Route path="/logout" element={<Login/>}/>
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
-export default App
+export default App;
